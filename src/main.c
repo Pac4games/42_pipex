6f566973247ab37c54e00fe1f06a96b81db16c0b
Paulo Cesar Goncalves Nicolau <paugonca@c1r20s3.42lisboa.com>
2023-06-07 11:55:51 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main.c
+++ src/main.c
@@ -6,26 +6,44 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 14:57:28 by paugonca          #+#    #+#             */
-/*   Updated: 2023/06/01 17:09:41 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/06/07 11:34:40 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include <stdlib.h>
 
 int	main(void)
 {
-	int	id;
-	int	res;
-
-	id = fork();
-	if (id == 0)
-		sleep(1);
-	printf("Current ID: %d, Parent ID: %d\n", getpid(), getppid());
-	res = wait(NULL);
-	if (res == -1)
-		printf("No children to wait for.\n");
+	int	pid;
+	int	file;
+	int	wstatus;
+	int	statusCode;
+	
+	pid = fork();
+	file = open("pingResults.txt", O_WRONLY | O_CREAT, 0777);
+	wstatus = 0;
+	statusCode = 0;
+	if (pid == -1 || file == -1)
+		return (EXIT_FAILURE);
+	else if (pid == 0)
+	{
+		if (execlp("ping", "ping", "-c", "1", "google.com", NULL) == -1)
+		{
+			printf("Program not found!\n");
+			return (EXIT_FAILURE);
+		}
+	}
 	else
-		printf("%d finished execution.\n", res);
-	return (EXIT_SUCCESS);
+	{
+		wait(&wstatus);
+		if (WIFEXITED(wstatus))
+		{
+			statusCode = WEXITSTATUS(wstatus);
+			if (statusCode == EXIT_SUCCESS)
+				printf("Success!\n");
+			else
+				printf("Failure\n");
+		}
+	}
+	return (statusCode);
 }
