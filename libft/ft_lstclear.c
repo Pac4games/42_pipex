8414c0f260fcac4e0a90fff6eadeaa68c1e06b02
Paulo Cesar Goncalves Nicolau <paugonca@c1r19s4.42lisboa.com>
2023-06-09 17:29:44 +0100

    Norme error fix

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:50:29 by paugonca          #+#    #+#             */
-/*   Updated: 2022/10/27 12:36:53 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/06/09 17:28:09 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*hold;
-	
+
 	if (!lst || !*lst)
 		return ;
 	while (*lst)
